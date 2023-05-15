3ed24800fd0819fe38dfdb3f5282a927dba93d66
David Maple <david.maple@elitefintech.com>
2023-05-15 00:25:38 -0700

    testing

--- check_binary.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-check_binary() {
-  if ! which "$1" > /dev/null; then
-    ( >&2 echo "$2" )
-    exit 1
-  fi
-}
--- increment_semantic_file.sh
+++ increment_semantic_file.sh
@@ -2,6 +2,12 @@
 
 set -e
 
+# ensure we have wget
+if ! which "wget" > /dev/null; then
+    echo "wget is required to use increment_semantic_file.sh"
+    exit 1
+fi
+
 increment_operation="patch"
 while test $# -gt 0; do
   case "$1" in
@@ -21,12 +27,6 @@ if [ ! -f "$1" ]; then
     echo "Semantic version file: $1 does not exist."
     exit 1
 fi
-
-# ensure we have curl
-check_binary "curl" "$(cat <<EOF
-curl is required to use increment_semantic_file.sh
-EOF
-)"
 
 current_version="$(cat "$1")"
 
